98d88b7fec79b965d88630d605054f4798aece2f
ray <raynasari60.rn@gmail.com>
2022-09-03 11:31:19 +0300

    first commit

---
+++ listings/choices.py
@@ -0,0 +1,29 @@
+#bedroom_choices = {'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10}
+
+price_choices = {
+  '500':'Tsh/=500',
+  '2000':'Tsh/=2000',
+  '3000':'Tsh/=3000',
+  '4000':'Tsh/=4000',
+  '5000':'Tsh/=5000',
+  '6000':'Tsh/=6000',
+  '7000':'Tsh/=7000',
+  '8000':'Tsh/=8000',
+  '9000':'Tsh/=9000',
+  '10000':'Tsh/=1M+',
+}
+
+state_choices = {
+        'TMK': 'Temeke',
+        'P': 'Posta',
+        'KR': 'Kariakoo',
+        'Moro': 'Morogoro',
+        'Mwana': 'Mwananyama',
+        'Ub': 'Ubungo',
+        'MB': 'Mbezi',
+        'TB': 'Tabata',
+        'BG': 'Buguruni',
+        'Bz': 'Buza',
+        'TK': 'Tandika',
+        
+}
